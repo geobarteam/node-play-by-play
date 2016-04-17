fe9d209b1b92a209bbe8b10ff8673fb1a4b78562
Geobarteam <gvd8@hotmail.com>
2016-04-17 14:48:49 +0200

    hello worl express

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-var http = require('http')
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res){
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World\n');
-    
-}).listen(3000, '127.0.0.1');
+app.get('/', function(req,res){
+    res.send('Hello: world');
+});
 
-console.log('Server running at http://127.0.0.1:3000/');
+var server = app.listen(3000, function () {
+    console.log('Server running at http://127.0.0.1:3000/');
+});
