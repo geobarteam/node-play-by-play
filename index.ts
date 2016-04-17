5e65aaf24d85c39239f62610866de8cb65b71064
Geobarteam <gvd8@hotmail.com>
2016-04-17 16:16:14 +0200

    typescript configured

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-"use strict";
-var express = require("express");
+import * as express from "express";
+
 var http = require('http');
 var app = express();
-app.get('/', function (req, res) {
+
+app.get('/', function(req,res){
     res.send('Hello: world');
 });
+
+
 var server = app.listen(3000, function () {
     console.log('Server running at http://127.0.0.1:3000/');
 });
-//# sourceMappingURL=index.js.map
